c4aad36d104a8954d22a889677236f3df796d42b
YASH BOTHRA <yash.bothra@pramayo.co>
2026-03-15 21:28:59 +0530

    n

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://simplitask-pro-930y.onrender.com"
+      "https://scheduled-management-system.vercel.app"
     ]
   })
 );
